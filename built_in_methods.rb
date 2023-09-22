136d82157ecadd829b7c9d0641f033cd5ffdf711
Yain Porter <yainporter@gmail.com>
2023-09-22 10:16:11 -0700

    Complete the work in built_in_methods.rb

--- built_in_methods.rb
+++ built_in_methods.rb
@@ -8,19 +8,29 @@
 # The return value is "hello world"
 "Hello World".downcase
 
-
+# The include? method is called on the string object "Hello World"
+# The argument "Hello" is passed; include? checks the string object for the argument passed to see if it's included
+# The return value is true
 "Hello World".include?("Hello")
 
-
+# The end_with? method is called on the string object "Hello World"
+# The argument "Hello" is passed; end_with? checks the end of the string object for the argument
+# The return value is false
 "Hello World".end_with?("Hello")
 
-
+# The end_with? method is called on the string object "Hello World"
+# The argument "rld" is passed; end_with? checks the end of the string object for the argument
+# The return value is true
 "Hello World".end_with?("rld")
 
-
+# The even? method is called on the integer object 12
+# No arguments are passed; even? is a boolean method that checks to see if the integer is even
+# The return value is true
 12.even?
 
-
+# The next method is called on the integer object 18
+# No arguments are passed; next is a method that returns the next number following the integer object it's called on
+# The return value is 19
 18.next
 
 
@@ -30,6 +40,18 @@
 # Call a different built-in Ruby method on each of your variables. 
 # https://ruby-doc.org/core-3.1.0/String.html
 # Include comments above each method call explaining the impact and return value of that method.
+first_name = "Yain"
+last_name = "Porter"
+
+# The reverse method is called on the string variable first_name
+# No arguments are passed; reverse is a method that will reverse the string
+# The return value is "niaY"
+first_name.reverse
+
+# The upcase method is called on the string variable last_name
+# No arguments are passed; upcase is a method that turns all the letters in the string to uppercase
+# The return value is "PORTER"
+last_name.upcase
 
 # EXAMPLE
 # The start_with? method is called on the first_name variable, which stores the string object "Jeff". 
@@ -46,11 +68,34 @@ puts first_name.start_with?("J")
 # Call a different built-in Ruby method on each of your variables. 
 # https://ruby-doc.org/core-3.1.0/Integer.html
 # Include comments above each method call explaining the impact and return value of that method.
+birth_year = 1991
+current_year = 2023
 
+# The to_s method is called on the birth_year variable, which stores the integer 1991.
+# The to_s method then turns the data in the variable birth_year into a string.
+# The return value of the to_s method called on the birth_year variable is now "1991"
+birth_year.to_s
 
+# The pred method is called on the current_year variable, which stores the integer 2023.
+# The pred method is used to return the previous consecutive integer.
+# The return value of the pred method called on the current_year variable is now 2022.
+current_year.pred
 
 # SECTION 4: Calling methods on variables assigned to arrays.
 # Declare 2 variables assigned to arrays.
 # Call a different built-in Ruby method on each of your variables. 
 # https://ruby-doc.org/core-3.1.0/Array.html
 # Include comments above each method call explaining the impact and return value of that method.
+
+names = ["Justice", "Luna", "Meeko", "Kida"]
+ages = [2, 5, 6, 6, 8]
+
+# The first method is called on the variable names, which stores an array with the elements: Justice, Luna, Meeko, and Kida.
+# The first method is used to select the first element in an array.
+# The return value of the first method being called on the names variable would be "Justice".
+names.first
+
+# The uniq method is called on the variable ages, which stores an array with the elements: 2, 5, 6, 6, 8.
+# The uniq method is used to check an array for duplicates, and return only the unique elements
+# The return value of the uniq method called on the variable ages would be: 2, 5, 6, 8.
+ages.uniq
